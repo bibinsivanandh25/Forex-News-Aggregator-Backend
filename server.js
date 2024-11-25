3f60c6d6bd3a002a7c7e4c359de86f0f34006aa1
bibinsivanandh25 <bibinsivanandh25@gmail.com>
2024-11-25 20:04:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import cors from 'cors';
+import axios from 'axios';
+import express, { json } from 'express';
+
+const app = express();
+const port = 5000;
+
+const BASE_URL = 'https://newsapi.org/v2/everything';
+const API_KEY = 'b59fac3a063e4125b1bd5dc3d885938f';
+
+app.use(cors());
+app.use(json());
+
+app.get('/api/news', async (req, res) => {
+  const { query, language } = req.query;
+  const lang = language || 'en';
+
+  if (!query) {
+    return res.status(400).json({ error: 'Query parameter is required' });
+  }
+
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?q=${query}&apiKey=${API_KEY}$language=${lang}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
